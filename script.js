486c2723d3dd87391936cf0b25287d996b671d12
taisaangelucci1 <167939106+taisaangelucci1@users.noreply.github.com>
2025-09-12 21:42:14 -0300

    tt

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.getElementById("btnCadastrar").addEventListener("click", function() {
+  alert("Cadastro iniciado!");
+  // Aqui você pode redirecionar para outra página, por exemplo:
+  // window.location.href = "cadastro.html";
+});
 
